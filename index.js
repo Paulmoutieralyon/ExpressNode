cd0178e1bb49a0e09050b4af7f228880cfb63db2
Paul Moutier <moutier.paul@gmail.com>
2018-11-12 16:39:27 +0100

    deux

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const  express  =  require ( ' express ' );
+const  app  =  express ();
+ port  =  3000 ;
 
-app.get('/', (request, response) => {
-    response.send('Bienvenue sur Express');
+app . get ( ' / ' , ( demande , réponse ) => {
+  réponse . envoyer ( ' Bienvenue sur Express ' );
 });
 
-app.listen(port, (err) => {
-    if (err) {
-        throw new Error('Something bad happened...');
-    }
+app . get ( ' / api / movies ' , ( demande , réponse ) => {
+  réponse . envoyer ( ' Récupération de tous les films ' );
+});
 
-    console.log(`Server is listening on ${port}`);
+app . get ( ' / api / movies /: id ' , ( demande , réponse ) => {
+  const  id  =  demande . params . id ;
+  réponse . json ({id : id});
+});
+
+app . get ( ' / api / employee ' , ( demande , réponse ) => {
+  réponse . sendStatus ( 304 );
+});
+
+app . get ( ' / api / employee /: name ' , ( demande , réponse ) => {
+  réponse . statut ( 404 ). send ( ` impossible de trouver réutilisé $ { request . params . name } ` )
+
+});
+
+app . listen (port, ( err ) => {
+  si (err) {
+    jeter une  nouvelle  erreur ( ' Quelque chose de mauvais est arrivé ... ' );
+  }
+
+  console . log ( `le serveur est à l’écoute sur $ { port } ` );
 });
