66642b5ff56d52eef8af8ae7f294450760ab9a9f
Paul Moutier <moutier.paul@gmail.com>
2018-11-17 13:40:10 +0100

    Last version

--- index.js
+++ index.js
@@ -2,27 +2,35 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/api/movies', (request, response) => {
-    response.send('Récupération de tous les films');
-});
-
-app.get('/api/movies/:id', (request, response) => {
-    const id = request.params.idFilm;
-    response.json({ id: id})
-})
-
-app.get('/api/employee', (resquest, response) => {
-    response.sendStatus(304);
-})
-
-app.get('/api/employee', (request, response) => {
-    const name = request.query.name;
-    response.status(404).send('Impossible de récupérer l\'employé ' + name)
-})
-
 app.listen(port, (err) => {
     if (err) {
         throw new Error('Something bad happened...');
     }
     console.log(`Server is listening on ${port}`);
+});
+
+app.get('/', (request, response) => {
+    response.send('Hellow world !')
+})
+app.get('/api/movies', (request, response) => {
+    response.send('Récupération des films');
+});
+app.get('/api/movies/:id', (request, response) => {
+    console.log("movies")
+    response.json(request.params.id)
+});
+
+
+
+
+app.get('/api/employee', (request, response) => {
+    let id = request.query.id
+    console.log(id)
+    if (id !== undefined) {
+        response.status(404).send("Impossible de récupérer l'employé " + id);
+    }
+    else {
+        response.status(304).end()
+    }
+
 });
