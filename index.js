6883ffa7c05ad8708eac707449b04f73510e078b
Paul Moutier <42945383+Paulmoutieralyon@users.noreply.github.com>
2018-12-10 10:08:27 +0100

    update

--- index.js
+++ index.js
@@ -1,85 +1,36 @@
-const connection = require('./conf');
 const express = require('express');
-const bodyParser = require('body-parser');
-const express = require('express') ; 
-const router = express.Router();
-const multer = require('multer');
-const upload = multer({dest: 'tmp/'});
-
 const app = express();
 const port = 3000;
+const connection = require('./conf');
 
-// Support JSON-encoded bodies
-app.use(bodyParser.json());
-// Support URL-encoded bodies
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+const multer = require('multer');
 
-
-app.post('/uploaddufichier', upload.array('monfichier'), function (req, res, next) {
-  // traitement du formulaire
-})
-
-app.post('/uploaddufichier', upload.single('monfichier'), function (req, res, next) {
-  fs.rename(req.file.path, 'public/images/' + req.file.originalname, function(err){
-    if (err) {
-        res.send('problème durant le déplacement');
-    } else {
-        res.send('Fichier uploadé avec succès');
-    }
-  });
-})
-
-
-
-
-
-
-// ////////////// Méthode DELETE sur MOVIES
-// app.delete('/api/movies/:id', (req, res) => {
-//   const idMovie = req.params.id;
-//   // 
-//   connection.query('DELETE FROM movie WHERE id = ?', [idMovie], err => {
-//     // 
-//     if (err) {
-//       console.log(err);
-//       res.status(500).send("Erreur lors de la suppression d'un employé");
-//     } else {
-//       res.sendStatus(200);
-//     }
-//   });
-// });
-
-// app.listen(port, (err) => {
-//   if (err) {
-//     throw new Error('Something bad happened...');
-//   }
-
-//   console.log(`Server is listening on ${port}`);
-// });
-// app.post('/api/movies', (req, res) => {
-//   // récupération des données envoyées
-//   const formData = req.body;
-
-//   // connexion à la base de données, et insertion de l'employé
-//   connection.query('INSERT INTO movie SET ?', formData, (err, results) => {
-
-//     if (err) {
-//       // Si une erreur est survenue, alors on informe l'utilisateur de l'erreur
-//       console.log(err);
-//       res.status(500).send("Erreur lors de la sauvegarde d'un employé");
-//     } else {
-//       // Si tout s'est bien passé, on envoie un statut "ok".
-//       res.sendStatus(200);
-//     }
-//   });
-// });
-
-app.listen(port, (err) => {
-  if (err) {
-    throw new Error('Something bad happened...');
+const storage = multer.diskStorage({
+  destination: function(req,file,cb){
+    cb(null,'public/')
+  },
+  filename :function(req,file,cb){
+    cb(null,file.originalname)
   }
+})
 
-  console.log(`Server is listening on ${port}`);
+const upload = multer({
+  dest: 'tmp/',
+  storage,
+  limits :{
+    fileSize : 3000000
+  },
+  fileFilter: (req,file,cb) =>{
+    if(!file.mimetype.includes('image/png'))
+    cb(new Error('Mauvais format de fichier'))
+
+    cb(null,true);
+ }
+})
+
+const fs = require('fs');
+
+app.post('/monupload', upload.array('monfichier',3),(req,res) =>{
+  console.log(req.files);
+  res.end();
 });
