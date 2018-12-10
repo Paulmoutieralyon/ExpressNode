0ac5c6310711a7c440b3b4e12cf45cd03830769f
Paul Moutier <moutier.paul@gmail.com>
2018-12-10 10:04:45 +0100

    file upload

--- index.js
+++ index.js
@@ -17,45 +17,64 @@ app.use(bodyParser.urlencoded({
 }));
 
 
-////////////// Méthode DELETE sur MOVIES
-app.delete('/api/movies/:id', (req, res) => {
-  const idMovie = req.params.id;
-  // 
-  connection.query('DELETE FROM movie WHERE id = ?', [idMovie], err => {
-    // 
+app.post('/uploaddufichier', upload.array('monfichier'), function (req, res, next) {
+  // traitement du formulaire
+})
+
+app.post('/uploaddufichier', upload.single('monfichier'), function (req, res, next) {
+  fs.rename(req.file.path, 'public/images/' + req.file.originalname, function(err){
     if (err) {
-      console.log(err);
-      res.status(500).send("Erreur lors de la suppression d'un employé");
+        res.send('problème durant le déplacement');
     } else {
-      res.sendStatus(200);
+        res.send('Fichier uploadé avec succès');
     }
   });
-});
+})
 
-app.listen(port, (err) => {
-  if (err) {
-    throw new Error('Something bad happened...');
-  }
 
-  console.log(`Server is listening on ${port}`);
-});
-app.post('/api/movies', (req, res) => {
-  // récupération des données envoyées
-  const formData = req.body;
 
-  // connexion à la base de données, et insertion de l'employé
-  connection.query('INSERT INTO movie SET ?', formData, (err, results) => {
 
-    if (err) {
-      // Si une erreur est survenue, alors on informe l'utilisateur de l'erreur
-      console.log(err);
-      res.status(500).send("Erreur lors de la sauvegarde d'un employé");
-    } else {
-      // Si tout s'est bien passé, on envoie un statut "ok".
-      res.sendStatus(200);
-    }
-  });
-});
+
+
+// ////////////// Méthode DELETE sur MOVIES
+// app.delete('/api/movies/:id', (req, res) => {
+//   const idMovie = req.params.id;
+//   // 
+//   connection.query('DELETE FROM movie WHERE id = ?', [idMovie], err => {
+//     // 
+//     if (err) {
+//       console.log(err);
+//       res.status(500).send("Erreur lors de la suppression d'un employé");
+//     } else {
+//       res.sendStatus(200);
+//     }
+//   });
+// });
+
+// app.listen(port, (err) => {
+//   if (err) {
+//     throw new Error('Something bad happened...');
+//   }
+
+//   console.log(`Server is listening on ${port}`);
+// });
+// app.post('/api/movies', (req, res) => {
+//   // récupération des données envoyées
+//   const formData = req.body;
+
+//   // connexion à la base de données, et insertion de l'employé
+//   connection.query('INSERT INTO movie SET ?', formData, (err, results) => {
+
+//     if (err) {
+//       // Si une erreur est survenue, alors on informe l'utilisateur de l'erreur
+//       console.log(err);
+//       res.status(500).send("Erreur lors de la sauvegarde d'un employé");
+//     } else {
+//       // Si tout s'est bien passé, on envoie un statut "ok".
+//       res.sendStatus(200);
+//     }
+//   });
+// });
 
 app.listen(port, (err) => {
   if (err) {
